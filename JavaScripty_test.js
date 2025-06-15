8acf6e56ac3a47a512b12676d5cc8f36373dc3c4
ajitsharma356 <47855498+ajitsharma356@users.noreply.github.com>
2025-06-15 22:47:39 +0530

    Second Commit

--- JavaScripty_test.js
+++ JavaScripty_test.js
@@ -1,6 +1,7 @@
 // This function prints "Hello, World!" to the console
 function greet() {
     console.log("Hello, World!");
+    consol.l("hello");
 }
 
 // Call the function to display the message
